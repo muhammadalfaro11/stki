b6c25fd15ddfaff4998632d62527489a13b438d3
muhammadalfaro11 <56127110+muhammadalfaro11@users.noreply.github.com>
2020-10-19 22:33:41 +0700

    uts

--- koneksi.php
+++ koneksi.php
@@ -2,11 +2,10 @@
 //include 'index.php';
 
 $host  = "localhost";
-$username  = "root";
-$password  = "";
+$username  = "root";$password = "" ;
 $database  = "nba";
-$koneksi  = mysqli_connect($host,  $username,  $password, $database);
-$pilihdatabase  = mysqli_select_db($koneksi, "stki");
+$koneksi  = mysqli_connect($host,  $username, $password, $database);
+$pilihdatabase  = mysqli_select_db($koneksi, "nba");
 if  ($pilihdatabase) echo "<h3>Koneksi Berhasil </h3><hr><br/>";
 else echo "Koneksi Gagal <br/>";
 ?>
